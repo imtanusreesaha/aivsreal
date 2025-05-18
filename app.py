f84bf7514f09e7dfc9185122b47f681f2cb6680b
Tanusree Saha <107751402+imtanusreesaha@users.noreply.github.com>
2025-05-19 00:41:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1zQmOz3Ep327nLr5yOutahPsA93aClgiM
 """
 
-!pip install streamlit
-
-!pip install streamlit
 import streamlit as st
 import tensorflow as tf
 import numpy as np
